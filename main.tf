7f4eb9db974f1d318c4400ceab33cb3710a5bb67
canblt38 <56888098+canblt38@users.noreply.github.com>
2021-08-03 15:39:29 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ terraform {
 
 provider "aws" {
   region  = "us-east-1"
-    access_key = "ASIAUYKDKOE7TOKSTDDJ"
-    secret_key = "n61XGHIucWfAUh7VXYpDMmQHxqSO2L57bxFpNU8B"
-    token = "FwoGZXIvYXdzEA8aDEaFbSzvX35v41F4EiLKAW9L3tjg5nYcHmd/FuhVODmC9lRfJQfaPZJPeufdkA8L6fsctmo8QKc0fXyXoG6n4vo0035AXMoPjONBgVFBFgPiVWWqHOLpbuwnUP/F7ER5UBoi/YL+zTgp8RQyn7Wub1rfdjo0XEz/z/30J7HkcTEOP2pKdHnd5xCt4bkcntoY/IwmablaDd6aD+bq91D4wrSIVsmMt4wDfMA2vS167FxEo7iqCFfam/SE4OmvL66i1vIp8APfh+QjK9xNZWyr+vNNaxL41EIA+g4o9IOliAYyLQGzUkjqUhvt0RgNpWZc1byARYlQNE93TCAedmEqdeM+eq4YZ+L7vZdI626Tjw=="
+    access_key = "ASIAYUOZXJF4SMHQ6BU7"
+    secret_key = "gCCfK1UvRIthvwpQcjmU5E6bAVPAMNYK8Dz9uMYl"
+    token = "FwoGZXIvYXdzEA8aDBSxzDRKt4FEFJivAiLKAXecaq9WjcqbPSjqQW0KC9XWJfEPEqHK21GuSbeksap59DfqxN5GQkJrAXcWD8+FtlwEcEr2QVJHv+RMcVHCNe+hEeb2azMMuTyQbzfVQaLaf2tvjWsKAswNlNc2EhYxyN5JEMIOf9NsC+q3/BOswGS20JJi5jOixhHzvLVxKG9xFreSfnoYxuAQ0gZofxO27u5mmllb23z+inbGiP82A5VAJK+CMhrk0+sfFwXS/ZiW3+902PQAkvu2BygWT4g4a+o8VXmS+/+RbtQop42liAYyLZQ1wuB8E618vlgQ3lq23e1q1i+kTkFJwQh5IZ2lmL742qqFa7OVaUWOc26cHQ=="
 }
