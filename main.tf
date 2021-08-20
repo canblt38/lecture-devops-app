f1cbe9bc332f6b1d259c958569e475b3601b4e11
unknown <s72308@beuth-hochschule.de>
2021-08-20 12:59:51 +0200

    run pipe

--- main.tf
+++ main.tf
@@ -82,6 +82,14 @@ resource "aws_security_group" "custom-instance-sg" {
         security_groups = [aws_security_group.custom-elb-sg.id]
     }
 
+    ingress {
+        description      = "ssh from VPC"
+        from_port        = 22
+        to_port          = 22
+        protocol         = "tcp"
+        cidr_blocks       = ["0.0.0.0/0"]
+    }
+
   egress {
       from_port        = 0
       to_port          = 0
