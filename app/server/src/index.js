ee43e5dd9387683e1fb6d3e7d83bf8ed75c0ba42
unknown <s72308@beuth-hochschule.de>
2021-08-20 13:44:16 +0200

    prometheus

--- app/server/src/index.js
+++ app/server/src/index.js
@@ -8,6 +8,17 @@ const todoRoutes = require('./routes/todo');
 const userRoutes = require('./routes/user');
 const errorRoutes = require('./routes/error');
 const envRoute = require('./routes/env.js');
+// https://stackabuse.com/nodejs-application-monitoring-with-prometheus-and-grafana/
+const client = require('prom-client');
+const register = new client.Registry();
+client.collectDefaultMetrics({
+    app: 'node-application-monitoring-app',
+    prefix: 'node_',
+    timeout: 10000,
+    gcDurationBuckets: [0.001, 0.01, 0.1, 1, 2, 5],
+    register
+});
+
 let cookieParser = require('cookie-parser');
 
 const app = express();
@@ -40,6 +51,32 @@ app.use(envRoute);
 // NOTE: must be last one, because is uses a wildcard (!) that behaves aa
 // fallback and catches everything else
 app.use(errorRoutes);
+
+
+// Create a custom histogram metric
+const httpRequestTimer = new client.Histogram({
+    name: 'http_request_duration_seconds',
+    help: 'Duration of HTTP requests in seconds',
+    labelNames: ['method', 'route', 'code'],
+    buckets: [0.1, 0.3, 0.5, 0.7, 1, 3, 5, 7, 10] // 0.1 to 10 seconds
+  });
+
+  // Register the histogram
+  register.registerMetric(httpRequestTimer);
+
+// Prometheus metrics route
+app.get('/metrics', async (req, res) => {
+    // Start the HTTP request timer, saving a reference to the returned method
+    const end = httpRequestTimer.startTimer();
+    // Save reference to the path so we can record it when ending the timer
+    const route = req.route.path;
+
+    res.setHeader('Content-Type', register.contentType);
+    res.send(await register.metrics());
+
+    // End timer and add labels
+    end({ route, code: res.statusCode, method: req.method });
+  });
 
 
 (async function main(){
